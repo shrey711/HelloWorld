be2cf36160dc7bdbcb45e70f2fdc357e4eeeb653
Shrey Gupta <shrey4317@gmail.com>
2024-04-08 16:29:46 +0530

    Add project files.

---
+++ HelloWorld/Controllers/HelloWorld.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelloWorld.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloWorld : ControllerBase
+    {
+        [HttpGet] 
+        public IActionResult Hello_World() 
+        {
+            return Ok(new {Message="HelloWorld"});
+        }
+    }
+}
